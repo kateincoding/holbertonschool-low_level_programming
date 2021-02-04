517f8c5f1593023df4ebc36399393bebdc65d143
kateincoding <kateincoding@gmail.com>
2021-02-04 00:58:44 +0000

    compiling in C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* puts - prints a message
+*
+* Return:  always 0
+*/
+
+void puts(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* printf - prints a message, necessary add \n to create a new line
+*
+* Return:  always 0
+*/
+
+void printf(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
