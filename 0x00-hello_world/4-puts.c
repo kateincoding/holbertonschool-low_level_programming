d4b986cca1124b0953e32ac49538dcfb72ed978c
kateincoding <kateincoding@gmail.com>
2021-02-04 01:19:27 +0000

    changing to main

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
-* puts - prints a message
+* main - Entry point of a program
 *
+* Puts: print a message, the \n is default
 * Return:  always 0
 */
-void puts(void)
+void main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
