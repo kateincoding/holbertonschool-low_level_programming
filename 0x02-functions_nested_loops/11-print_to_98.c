f0021a6441a1b34513149f1b43724096abbb6b94
kateincoding <kateincoding@gmail.com>
2021-02-09 16:01:14 +0000

    passing betty style

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,10 +8,10 @@
  */
 void print_to_98(int n)
 {
-	while ( n < 98)
-		printf("%d, ",n++);
-	while ( n > 98)
-		printf("%d, ",n--);
+	while (n < 98)
+		printf("%d, ", n++);
+	while (n > 98)
+		printf("%d, ", n--);
 	if (n == 98)
-		printf("%d\n",n);
+		printf("%d\n", n);
 }
