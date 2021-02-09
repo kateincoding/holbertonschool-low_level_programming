e1eef65a6671f7f313d2c14e10ddd52552346d51
kateincoding <kateincoding@gmail.com>
2021-02-09 11:58:26 +0000

    3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+/**
+ * _islower - function that indicates if alphabet lowercase
+ * @c: Character to validate
+ * Return: 1 if is lowercase; 0 othewhise.
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
