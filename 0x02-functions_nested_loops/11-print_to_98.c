c8543578cc6d52f94fb1d1297c7391a378484b14
kateincoding <kateincoding@gmail.com>
2021-02-09 15:58:37 +0000

    adding 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *print_to_98 - function that print numbers between n and 98
+ *@n: input number
+ *Return: void
+ */
+void print_to_98(int n)
+{
+	while ( n < 98)
+		printf("%d, ",n++);
+	while ( n > 98)
+		printf("%d, ",n--);
+	if (n == 98)
+		printf("%d\n",n);
+}
