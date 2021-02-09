c7c5ad8cacb4b48768f58d2b2d3005f4b7a91a2f
kateincoding <kateincoding@gmail.com>
2021-02-09 13:01:57 +0000

    add 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - Generate the last digit of a number n
+ * @n: The number to be cheked
+ * Return: last digit
+ */
+int print_last_digit(int n)
+{
+	if (n < 0)
+		n = -n;
+	if (n >= 10)
+		n = n % 10;
+	_putchar(n + '0');
+	return (n);
+}
