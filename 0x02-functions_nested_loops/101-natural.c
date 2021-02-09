b3892dabcd0ea90c5158a4f39464ced9b6272eae
kateincoding <kateincoding@gmail.com>
2021-02-09 21:05:27 +0000

    add 101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*Return: always 0
+*/
+int main(void)
+{
+	int n;
+	int sum;
+
+	sum = 0;
+	for (n = 0; n < 1024; n++)
+	{
+		if (n % 3 || n % 5)
+			sum = sum + n;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
