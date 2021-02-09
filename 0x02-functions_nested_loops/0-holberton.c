b75a61d421c8661041d03f90dcc30759059115cc
kateincoding <kateincoding@gmail.com>
2021-02-09 11:57:14 +0000

    adding functions

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+* main -Entry ppint of programs that print Holberton with putchar
+*
+* Return: Always 0
+*/
+int main(void)
+{
+	int i;
+	char word[9] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
+
+	i = 0;
+	while (i <= 9)
+		_putchar(word[i++]);
+	_putchar('\n');
+	return (0);
+}
