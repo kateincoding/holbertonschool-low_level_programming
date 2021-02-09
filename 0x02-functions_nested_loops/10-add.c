bdae4ea651f486e6c920da29c554b05a286fb18c
kateincoding <kateincoding@gmail.com>
2021-02-09 15:27:49 +0000

    adding 10, sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * add - functions that sum the inputs
+ * @a: input of number a
+ * @b: second input of number
+ * Return: Result sum of a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
