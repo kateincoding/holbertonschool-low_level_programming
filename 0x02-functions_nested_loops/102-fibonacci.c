92d2da222a50ec99aa07d5493acd41b8e3297341
kateincoding <kateincoding@gmail.com>
2021-02-09 23:08:33 +0000

    what a heavy pain in the head! fibonacci in one fx

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+*main - program that prints the first 50 numbers of fibonacci
+*
+*Return: Always 0
+*/
+int main(void)
+{
+	int i;
+	long b;
+	long n0;
+	long n1;
+
+	b = 0;
+	n0 = 1;
+	printf("%zu, %zu, ", b, n0);
+	for (i = 3; i <= 49; i++)
+	{
+		n1 = b + n0;
+		printf("%zu, ", n1);
+		b = n0;
+		n0 = n1;
+	}
+	n1 = b + n0;
+	printf("%zu\n", n1);
+	return (0);
+}
