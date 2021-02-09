29eb79689cfe1023ff88a45a5a36e994dfeeaf08
kateincoding <kateincoding@gmail.com>
2021-02-09 20:46:52 +0000

    adding funtion of tables

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+
+/**
+ * print_times_table - Generate a table of n
+ * @n: number input to do a its table
+ * Return : void
+ */
+
+void print_times_table(int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= n; i++)
+	{
+		if ( n > 15 || n < 0)
+			break;
+		for (j = 0; j <= n; j++)
+		{
+			if (j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (j * i <= 99)
+				{
+					_putchar(' ');
+					if (j * i <= 9)
+						_putchar(' ');
+				}
+			}
+			if (j * i > 99)
+			{
+				_putchar (i * j / 100 + '0');
+				_putchar ((i * j - (i * j / 100) * 100) / 10 + '0');
+			}
+			else if (j * i > 9)
+				_putchar (i * j / 10  + '0');
+			_putchar (i * j % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
