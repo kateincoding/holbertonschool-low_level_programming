18226825faa057d0b6b992a432d9d2f01fa5ba83
kateincoding <kateincoding@gmail.com>
2021-02-10 03:41:28 +0000

    divide a long number in 2 parts

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,22 +9,33 @@ int main(void)
 {
 	int i;
 
-	float n1;
-	float n2;
-	float n3;
+	unsigned long int n1, n2, n3;
+	unsigned long int d1, d3, r1, r2;
 
 	n1 = 1;
 	n2 = 2;
-	printf("%.0f, ", n1);
-	printf("%.0f, ", n2);
+	printf("%lu, ", n1);
+	printf("%lu, ", n2);
 	for (i = 3; i <= 98; i++)
 	{
-		n3 = n1 + n2;
-		printf("%.0f", n3);
-			if (i != 98)
-				printf(", ");
-		n1 = n2;
-		n2 = n3;
+		if (n1 + n2 > 10000000000 || r2 > 0 || r1 > 0)
+                {
+                        d1 = (n1 + n2) / 10000000000;
+                        n3 = (n1 + n2) % 10000000000;
+                        d3 = r1 + r2 + d1;
+                        r1 = r2, r2 = d3;
+                        n1 = n2, n2 = n3;
+                        printf("%lu%010lu", r2, n2);
+                }
+		else
+		{
+			n3 = n1 + n2;
+			printf("%lu", n3);
+			n1 = n2;
+			n2 = n3;
+		}
+		if (i != 98)
+			printf(", ");
 	}
 	printf("\n");
 	return (0);
