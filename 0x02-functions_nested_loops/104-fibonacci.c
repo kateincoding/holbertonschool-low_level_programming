a979ff90f8ea02ec09be46c454b172cd09d806f3
kateincoding <kateincoding@gmail.com>
2021-02-10 03:53:36 +0000

    fixing with betty and initialize

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -12,21 +12,23 @@ int main(void)
 	unsigned long int n1, n2, n3;
 	unsigned long int d1, d3, r1, r2;
 
+	r1 = 0;
+	r2 = 0;
 	n1 = 1;
 	n2 = 2;
 	printf("%lu, ", n1);
 	printf("%lu, ", n2);
 	for (i = 3; i <= 98; i++)
 	{
-		if (n1 + n2 > 10000000000 || r2 > 0 || r1 > 0)
-                {
-                        d1 = (n1 + n2) / 10000000000;
-                        n3 = (n1 + n2) % 10000000000;
-                        d3 = r1 + r2 + d1;
-                        r1 = r2, r2 = d3;
-                        n1 = n2, n2 = n3;
-                        printf("%lu%010lu", r2, n2);
-                }
+		if ((n1 + n2 > 10000000000) || r2 > 0 || r1 > 0)
+		{
+			d1 = (n1 + n2) / 10000000000;
+			n3 = (n1 + n2) % 10000000000;
+			d3 = r1 + r2 + d1;
+			r1 = r2, r2 = d3;
+			n1 = n2, n2 = n3;
+			printf("%lu%010lu", r2, n2);
+		}
 		else
 		{
 			n3 = n1 + n2;
