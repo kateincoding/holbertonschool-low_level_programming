dce631f7a99f208c8030c892014f4d66ecd07666
Your Name <k.soto@pucp.pe>
2021-02-11 16:11:16 -0500

    adding lines

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,12 +8,16 @@
 void more_numbers(void)
 {
 	int i;
+	int j;
 
-	for (i = 0; i <= 14; i++)
+	for (j = 0; j <= 10; j++)
 	{
-		if (i > 9)
-			_putchar(i / 10 + '0');
-		_putchar(i % 10 + '0');
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+				_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
