f8fae6d64689fb06924e4bf698fd0521b8cb70ef
Your Name <k.soto@pucp.pe>
2021-02-11 14:35:37 -0500

    betty style

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -5,9 +5,9 @@
  * @c: number in ascii to evaluate
  * Return: (1) if c is a digit (0) otherwise
  */
-int _isdigit (int c)
+int _isdigit(int c)
 {
-	if ( c >= '0' && c <= '9')
+	if (c >= '0' && c <= '9')
 		return (1);
 	return (0);
 }
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * main - prints 10 times the numbers, from 0 to 14
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 14; i++)
+	{
+		if (i > 9)
+			_putchar(i / 10);
+		_putchar(i % 10);
+	}
+	_putchar(\n);
+}
