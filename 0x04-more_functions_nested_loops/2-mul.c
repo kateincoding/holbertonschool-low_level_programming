6c3655409c726bf04dce772aa1acdc45e852f6b1
Your Name <k.soto@pucp.pe>
2021-02-11 14:16:04 -0500

    uploading files

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * mul - product of two inputs
+ * @a: first number in ascii to evaluate
+ * @b: second number in ascii to evaluate
+ * Return: (1) if c iS uppercase (0) otherwise
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - Prints numbers since 0 to 9
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for(i = 0; i <= 9; i++)
+	{
+		_putchar(i + '0');
+		_putchar('\n');
+	}
+}
