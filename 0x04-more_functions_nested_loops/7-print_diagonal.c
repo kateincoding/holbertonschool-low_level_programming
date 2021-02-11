b49f94dbcfd3572793e26abe5b7c2d3f6baace42
Your Name <k.soto@pucp.pe>
2021-02-11 16:36:11 -0500

    7 finishes

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,17 +1,25 @@
 #include "holberton.h"
 
+/**
+ *print_diagonal - Print a diagonal of n lines
+ *@n: n times
+ *Return: void
+ */
 void print_diagonal(int n)
 {
-	if ( n > 0)
+	int i;
+	int j;
+
+	if (n > 0)
 	{
-		for (i = 0; i <= n; i++)
+		for (i = 1; i <= n; i++)
 		{
-			for ( j = 0; j <= i; i++ );
-				_putchar(' ')
-			_putchar('\');
+			for (j = 1; j <= i; j++)
+				_putchar(' ');
+			_putchar('\\');
 			_putchar('\n');
 		}
 	}
-	else 
-		_putchar('\n')
+	else
+		_putchar('\n');
 }
