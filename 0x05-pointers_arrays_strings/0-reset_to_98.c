7484f47de117d14c2e324c3384fe30c5365391f3
Your Name <k.soto@pucp.pe>
2021-02-16 12:03:03 -0500

    05

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - update the value of pointer
+ * @n: pointer of type integer
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+first time array and pointers
