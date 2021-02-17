abd92035a90c54969dedade241a0a20ca41fbd82
Your Name <k.soto@pucp.pe>
2021-02-17 00:12:08 -0500

    8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -27,13 +27,13 @@ void print_array(int *a, int n)
 	int len;
 
 	len = _strlenint(a);
-	if (len >= n)
+	if (len >= n && n > 0)
 	{
 		for (i = 0; i < n - 1; i++)
 		{
 			printf("%d, ", a[i]);
 		}
-		printf("%d", a[i]);
+		printf("%d", a[n - 1]);
 	}
 	printf("\n");
 }
