359cac672fd6c2c2268c98d027ae08b71dae0c15
Your Name <k.soto@pucp.pe>
2021-02-17 00:19:19 -0500

    fixing

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,8 +11,14 @@ char *_strcpy(char *dest, char *src)
 	int i;
 
 	i = 0;
-	while (src[i++])
-		dest[i] = src[i];
+	if (src[i] != '\0')
+	{
+		while (src[i])
+		{
+			dest[i] = src[i];
+			i++;
+		}
+	}
 	dest[i] = '\0';
 	return (dest);
 }
