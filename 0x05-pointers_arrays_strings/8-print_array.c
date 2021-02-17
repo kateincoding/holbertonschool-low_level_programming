c7257d7f3bd0bd8a7c1cce16fefc27e7f732013c
Your Name <k.soto@pucp.pe>
2021-02-17 00:00:56 -0500

    h

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,6 +2,20 @@
 #include <stdio.h>
 
 /**
+*_strlen - returns the length of a string
+*@s: array
+*Return: length
+*/
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	return (i);
+}
+
+/**
 *print_array - print each character of array with , ' '
 *@a: array
 *@n: number of parameters to print
@@ -10,10 +24,16 @@
 void print_array(int *a, int n)
 {
 	int i;
+	int len;
 
-	for (i = 0; i < n - 1; i++)
+	len = _strlen(a)
+	if (len >= n)
 	{
-		printf("%d, ", a[i]);
+		for (i = 0; i < n - 1; i++)
+		{
+			printf("%d, ", a[i]);
+		}
+		printf("%d", a[i]);
 	}
-	printf("%d\n", a[i]);
+	printf("\n")
 }
