680f7a92f3b055fbc853ca2f44d8c8e6473b066f
Your Name <k.soto@pucp.pe>
2021-02-16 19:43:49 -0500

    adding funciones and include and putchar

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,8 +1,11 @@
 #ifndef HOLBERTON_H
 # define HOLBERTON_H
+#include <unistd.h>
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void _puts(char *str);
+void _putchar(char c);
 
 #endif
