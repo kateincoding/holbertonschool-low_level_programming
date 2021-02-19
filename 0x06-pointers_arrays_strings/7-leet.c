5a49a38775f185e4cb198635d1ece57800ba1e99
Your Name <k.soto@pucp.pe>
2021-02-19 00:46:41 -0500

    betty style

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,7 +2,7 @@
 
 /**
  * leet - encodes a string 1337
- * s: string to encode
+ * @s: string to encode
  * Return: s encoded
  */
 char *leet(char *s)
@@ -10,7 +10,7 @@ char *leet(char *s)
 	int i = 0, j = 0;
 	char letters[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 	char encode[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
-	
+
 	while (s[i])
 	{
 		for (j = 0; letters[j]; j++)
