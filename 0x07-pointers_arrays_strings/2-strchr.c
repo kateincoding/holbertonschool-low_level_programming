4faa5c5e6b6cc5aa1eb67c7921b7e5252f95254f
Your Name <k.soto@pucp.pe>
2021-02-22 14:38:10 -0500

    second

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _strchr: find the char in string
+ * @s: string
+ * @c: char
+ * Return: pointer to the rest od string
+ */
+char *_strchr(char *s, char c)
+{
+	int size_l = 0, i = 0;
+	char *p;
+
+	while (s[i])
+	{
+		if (c == s[i])
+		{
+			p = s + i;
+			break;
+		}
+		p = 0;
+		size_l++;
+	}else
+		p = 0;
+	return (p);
+}
