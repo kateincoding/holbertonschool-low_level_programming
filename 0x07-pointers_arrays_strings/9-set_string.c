fc06c1bbc28c422e98189905bafb9667e36fdd63
Your Name <k.soto@pucp.pe>
2021-02-22 23:31:37 -0500

    connect with a pointer

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * set_string - sets the value of a pointer s to a (to)
+ * @s: address of pointer
+ * @to: address of value
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
