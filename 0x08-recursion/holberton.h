ae18d504b2eac302762e1779c08760b6e3eb5891
Your Name <k.soto@pucp.pe>
2021-02-24 14:56:56 -0500

    first

---
+++ 0x08-recursion/holberton.h
@@ -0,0 +1,10 @@
+
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+void _puts_recursion(char *s);
+void _putchar(int c);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+
+#endif
