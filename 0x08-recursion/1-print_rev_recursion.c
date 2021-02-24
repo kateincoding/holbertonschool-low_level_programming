0466c49a2c74bd645da14629e1175592ac698e1a
Your Name <k.soto@pucp.pe>
2021-02-24 15:40:32 -0500

    rev_recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - prints in reverse
+ * @s: string to print
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
