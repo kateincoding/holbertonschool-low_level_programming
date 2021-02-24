bfe2d643e363839ffbe9f08b401f76e64ae727bf
Your Name <k.soto@pucp.pe>
2021-02-24 15:56:03 -0500

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * factorial = factorial of number n
+ * @n: number
+ * Return: factorial
+ */
+int factorial(int n)
+{
+	if (n == 1 || n == 0)
+		return (1);
+	if (n < 0)
+		return (-1);
+	else
+		return (n * factorial(n - 1));
+}
