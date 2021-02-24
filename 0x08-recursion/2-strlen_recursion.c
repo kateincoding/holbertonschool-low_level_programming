838577b1be1db25838eb0dc6606e511435eb1ef3
Your Name <k.soto@pucp.pe>
2021-02-24 15:46:32 -0500

    prints

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - Return length
+ * @s: string
+ * Return: length / 0 if s is empty
+ */
+int _strlen_recursion(char *s)
+{
+	while (*s)
+		return(1 + _strlen_recursion(s + 1));
+	return (0);
+}
