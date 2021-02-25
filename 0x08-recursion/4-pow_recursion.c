c920527eb13b619563765095560c28764cb58360
Your Name <k.soto@pucp.pe>
2021-02-24 21:02:32 -0500

    ex5

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,6 @@
+#include "holberton.h"
+
+int _pow_recursion(int x, int y)
+{
+	
+}
---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * sqrt_modify - with 2 input
+ * @n: number
+ * @i: number to iterate
+ * Return: square root
+ */
+int sqrt_modify(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+	else if (i * i > n)
+		return (-1);
+	return (sqrt_modify(n, i + 1));
+}
+
+/**
+ * _sqrt_recursion - search square root of n
+ * @n = number
+ * Return: square root of n
+ */
+int _sqrt_recursion(int n)
+{
+	return (sqrt_modify(n,1));
+}
