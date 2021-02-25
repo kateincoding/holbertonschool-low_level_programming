3143c2d76f7ffbf3546ae2f42f9101135fe270ad
Your Name <k.soto@pucp.pe>
2021-02-24 21:45:59 -0500

    pow

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,6 +1,16 @@
 #include "holberton.h"
 
+/**
+ * _pow_recursion - function that pows
+ * @x: number
+ * @y: exponent
+ * Return: pow of x
+ */
 int _pow_recursion(int x, int y)
 {
-	
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
 }
