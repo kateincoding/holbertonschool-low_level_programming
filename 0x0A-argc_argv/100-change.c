d2e90f0cd178b16c4dd9d6d3c8bf092ede64f039
Your Name <k.soto@pucp.pe>
2021-02-28 23:53:55 -0500

    100

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Calculate the cents to return
+ * @argc: number of argv
+ * @argv: money
+ * Return: 0, true; Error is 1
+ */
+int main(int argc, char **argv)
+{
+	int cents[] = {25, 10, 5, 2, 1};
+	int i, j, money, result = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	money = atoi(argv[1]);
+	if (money < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 0; i <= 4; i++)
+	{
+		if (money == 0)
+		{
+			printf("%d\n", result);
+			return (0);
+		}
+		if (cents[i] <= money)
+		{
+			result += money / cents[i];
+			money = abs(money - (cents[i] * result));
+		}
+		for (j = 0; j < 5; j++)
+		{
+			if (money == cents[j])
+			{
+				result += 1;
+				money = 0;
+			}
+		}
+	}
+	printf("%d\n", result);
+	return (0);
+}
