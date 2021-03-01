6712ab2d1376fad7fd612d1e4b5ec495bfba7286
Your Name <k.soto@pucp.pe>
2021-02-28 22:30:30 -0500

    Readme and name of program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints argv
+ * @argc: number of argv
+ * @argv: commands of program
+ * Return: Always 0
+ **/
+int main(int argc, char **argv)
+{
+	argc = 0;
+
+	printf("%s\n", argv[argc]);
+	return (0);
+}
