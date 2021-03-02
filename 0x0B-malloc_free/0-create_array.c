e70112bc9a80e1f4ee141b14ff38a41a8e15708e
kateincoding <kateincoding@gmail.com>
2021-03-02 23:00:22 +0000

    create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,9 +14,10 @@ char *create_array(unsigned int size, char c)
 
 	s = malloc(size * sizeof(char));
 
-	if (size == 0 || s = NULL)
+	if (size == 0)
 		return (NULL);
 	for (i = 0; i < size ; i++)
 		*(s + i) = c;
+	*(s + i) = '\0';
 	return (s);
 }
