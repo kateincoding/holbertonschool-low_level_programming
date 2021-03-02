afdaae3f6af9377128f30cafec1075626c7513d0
kateincoding <kateincoding@gmail.com>
2021-03-02 23:59:39 +0000

    copy a string and return a pointer

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - duplicate the string str and return a pointer
+ * @str: string to duplicate
+ * Return: pointer to copy of string
+ */
+char *_strdup(char *str)
+{
+	int i = 0;
+	char *p;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[i])
+		i++;
+	p = malloc(sizeof(char) * i + 1);
+	i = 0;
+	while (str[i])
+	{
+		p[i] = str[i];
+		i++;
+	}
+	p[i] = '\0';
+	return (p);
+}
