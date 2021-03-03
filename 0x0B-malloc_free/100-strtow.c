bc8bd678879045425f52b7f769cc9889ae156579
kateincoding <kateincoding@gmail.com>
2021-03-03 07:04:13 +0000

    finally less than 40 lines per function

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,86 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+
+/**
+ * _calculate_word_height - calculate height of array for then allocate memory
+ * @str: str to evaluate and count words
+ *
+ * Return: word_height
+ */
+int _calculate_word_height(char *str)
+{
+	int i = 0, length = 0;
+	int word_height = 0, flag_word = 0;
+
+	/*First, calculate length: word +\n + \0 */
+	while (str[i])
+	{
+		flag_word = 0;
+		while (str[i] == ' ')
+			i++;
+		while (str[i] >= 33 && str[i] <= 126)
+		{
+			i++;
+			length++;
+			flag_word = 1;
+		}
+		/* we identify how many words (number of \n) & add space for \n */
+		if (flag_word == 1)
+		{
+			length++;
+			word_height++;
+		}
+	}
+	return (word_height + 1);
+}
+
+/**
+ * strtow - create an array of word in a string
+ * @str: string to evaluate
+ * Return: bi-mensional array
+ */
+
+char **strtow(char *str)
+{
+	int i = 0, j = 0, k, l, word_height, word_width, flag_word = 0;
+	char **arr;
+
+	word_height = _calculate_word_height(str);
+	/*Allocate space for row of arr*/
+	arr = malloc(sizeof(char *) * word_height);
+	if (arr == 0)
+		return (NULL);
+	/*allocates in a loop for each columns*/
+	while (str[i])
+	{
+		flag_word = 0;
+		while (str[i] == ' ')
+			i++;
+		word_width = 0;
+		while (str[i + word_width] >= 33 && str[i + word_width] <= 126)
+		{
+			word_width++;
+			flag_word = 1;
+		}
+		/* we identify how many words (number of \n) & add space for \n */
+		if (flag_word == 1)
+		{
+			*(arr + j) = malloc((word_width + 1) * sizeof(char));
+			if (*(arr + j) == 0)
+			{
+				for (k = 0; k <= i; k++)
+					free(*(arr + k));
+				free(arr);
+				return (NULL);
+			}
+			/*creating the array*/
+			for (l = 0; l < word_width; l++)
+				arr[j][l] = str[i++];
+			arr[j][l] = '\0';
+			if (j <= word_height)
+				j++;
+		}
+	}
+	return (arr);
+}
