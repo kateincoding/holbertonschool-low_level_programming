0f9d3c7884ef7362b542e83f3f2d739c8a574d4b
kateincoding <kateincoding@gmail.com>
2021-03-03 02:37:33 +0000

    error before: inicializate in while

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,6 +12,11 @@ char *str_concat(char *s1, char *s2)
 	int i = 0, j = 0, len1 = 0, len2 = 0;
 	char *p;
 
+	if (s1 = NULL)
+		*s1 = "";
+	if (s2 = NULL)
+		*s2 = "";
+
 	while (s1[len1])
 		len1++;
 	while (s2[len2])
