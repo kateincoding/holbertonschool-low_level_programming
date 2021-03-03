3ceb4ef66851904c60d916b875952392b455231a
kateincoding <kateincoding@gmail.com>
2021-03-03 02:32:11 +0000

    s2 reestr

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,9 +12,6 @@ char *str_concat(char *s1, char *s2)
 	int i = 0, j = 0, len1 = 0, len2 = 0;
 	char *p;
 
-	if (s1 == NULL && s2 == NULL)
-		return (NULL);
-
 	while (s1[len1])
 		len1++;
 	while (s2[len2])
@@ -24,9 +21,11 @@ char *str_concat(char *s1, char *s2)
 
 	if (p == NULL)
 		return (NULL);
+
 	for (i = 0; i < len1; i++)
 		*(p + i) = s1[i];
-	for (j = 0; j <= len2; j++)
-		*(p + i + j) = s2[j];
+	i = 0;
+	for (j = len1; j <= len2; j++)
+		*(p + j) = s2[i];
 	return (p);
 }
