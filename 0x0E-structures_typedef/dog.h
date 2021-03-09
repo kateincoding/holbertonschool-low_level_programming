7ef1fd53e5d468919ac2b92d7d8f92df85f43a23
kateincoding <kateincoding@gmail.com>
2021-03-09 04:52:52 +0000

    print-dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,5 +19,7 @@ struct dog
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+void dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
