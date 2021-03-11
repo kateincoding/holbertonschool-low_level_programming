26720bff3335f7b3eb8eb224213297539c10b67d
kateincoding <kateincoding@gmail.com>
2021-03-11 06:09:03 +0000

    difficult, finally I need to use strcmp to erase other characters

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -7,18 +7,21 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-	op_t ops[] = {
-	{"+", op_add},
-	{"-", op_sub},
-	{"*", op_mul},
-	{"/", op_div},
-	{"%", op_mod},
-	{NULL, NULL}
-	};
 	int i = 0;
 
-	while ((ops[i].op[0] != s[i]) && i <= 5)
-		i++;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}};
 
-	return (ops[i].f);
+	while (i < 5)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+			return (ops[i].f);
+		i++;
+	}
+	printf("Error\n"), exit(99);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,19 +8,17 @@
  */
 int main(int argc, char **argv)
 {
-	int (*fx)(int, int);
+	int a, b, result;
 
 	if (argc != 4)
 		printf("Error\n"), exit(98);
 
-	if ((argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '*'
-	     && argv[2][0] != '/' && argv[2][0] != '%') || strlen(argv[2]) != 1)
-		printf("Error\n"), exit(99);
-
-	if ((argv[2][0] == '/' || argv[2][0] == '%') && (atoi(argv[3]) == 0))
+	if ((*argv[2] == '/' || *argv[2] == '%') && *argv[3] == '0')
 		printf("Error\n"), exit(100);
 
-	fx = get_op_func(argv[2]);
-	printf("%d\n", fx(atoi(argv[1]), atoi(argv[3])));
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	result = get_op_func(argv[2])(a, b);
+	printf("%i\n", result);
 	return (0);
 }
