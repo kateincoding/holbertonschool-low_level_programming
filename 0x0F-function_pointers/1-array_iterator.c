e2fc0ed1b7afdefe97675c4a45fd100abb7d5b65
kateincoding <kateincoding@gmail.com>
2021-03-11 03:57:15 +0000

    adding functions

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - fx that executes a fx given as a parameter on each element of an array
+ * @array: array
+ * @size: size
+ * @action: pointer to function that prints the array
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (action && array)
+	{
+		for (i = 0; i < size; i++)
+			(*action)(array[i]);
+	}	
+}
---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - return the index of first element when cmp != 0
+ * @array: array
+ * @size: number of element in the array
+ * @cmp: pointer to function to compare values
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && cmp && size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+				return(i);
+		}
+	}
+	return (-1);
+}
