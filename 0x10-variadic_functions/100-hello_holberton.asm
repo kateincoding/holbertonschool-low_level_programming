b83b87fa190456ae0b9488dc27497cdd842180e4
kateincoding <kateincoding@gmail.com>
2021-03-12 05:24:11 +0000

    asm

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -2,22 +2,10 @@ section .data
 	message db "Hello, Holberton", 10
 
 section .text
-	/*main fx for gcc*/
 	global main
 main:
-	/* 3arg: length of str */
+	mov rax, 17
+	mov rdi, 1
+	mov rsi, message
 	mov rdx, 17
-	/*2arg: pointer to msg to write */
-	mov rcx, msg
-	/*1st arg: file handle (stdout) */
-	mov rbx, 1
-	/* system call number sys_write */
-	mov rax, 4
-	/* call kernel */
-	int 0x80
-
-	/*move 0 to rbx*/
-	mov rbx, 0
-	 /*system call for exit */
-	mov rax, 1
-	int 0x80
+	syscall
