6bf253a4c4a6c1a1aa601e431c2b7176e2f0f604
kateincoding <kateincoding@gmail.com>
2021-03-19 01:26:04 +0000

    exercise 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - Print the lists
+ * @h: list with format list_t
+ * Return: number of nodes of the list
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned int number_nodes = 0;
+
+	while (h != NULL)
+	{
+		if (h->str)
+			printf("[%i] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
+		h = h->next;
+		number_nodes++;
+	}
+	return (number_nodes);
+}
