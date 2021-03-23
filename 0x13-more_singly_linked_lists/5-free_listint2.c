9943f71cdac617d94dfde09a4467b0c3f944fc06
kateincoding <kateincoding@gmail.com>
2021-03-23 15:25:57 +0000

    free a list and sets head to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free a LL and sets head to NULL
+ * @head: double pointer to head to allow sets head
+ * Return void
+ */
+void free_listint2(listint_t **head)
+{
+	if (*head == NULL || head == NULL)
+		return;
+	free_listint2(&(*head)->next);
+	free(*head);
+	*head = NULL;
+}
