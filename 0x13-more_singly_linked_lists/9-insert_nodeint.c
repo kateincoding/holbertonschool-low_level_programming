f942ad8e52e363f7ff013bd20d74737cf53e3181
kateincoding <kateincoding@gmail.com>
2021-03-23 20:11:45 +0000

    finish

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,13 +14,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (head == NULL || *head == NULL)
 		return (NULL);
-	tmp = *head;
 	if (idx == 0)
 	{
+		new_node = malloc(sizeof(listint_t));
+                        if (new_node == NULL)
+                                return (NULL);
 		new_node->next = *head;
 		*head = new_node;
 		return (new_node);
 	}
+	tmp = *head;
 	while (tmp->next)
 	{
 		if (counter == idx - 1)
