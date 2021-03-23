35f2c1921773a5857c1b9b8a074ae6455ed2072b
kateincoding <kateincoding@gmail.com>
2021-03-23 22:51:48 +0000

    9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -43,7 +43,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	/*case: add a node at the final */
 	if (counter == idx)
 	{
-		new_node->next = NULL;
 		prev_node->next = new_node;
 		return (new_node);
 	}
