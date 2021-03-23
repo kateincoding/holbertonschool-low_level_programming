bb1576a612f8a40916e30be1c550795bbfba7a6c
kateincoding <kateincoding@gmail.com>
2021-03-23 22:53:56 +0000

    with all restrictions

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -46,6 +46,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		prev_node->next = new_node;
 		return (new_node);
 	}
+	/*erase node when idx is not find it */
 	free(new_node);
 	return (NULL);
 }
