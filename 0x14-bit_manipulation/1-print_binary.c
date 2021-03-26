5c98cd31e2fe1e910f2c07e6ca463c1a9065631c
kateincoding <kateincoding@gmail.com>
2021-03-26 04:53:55 +0000

    print binaries

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * print_recursion - print with recursion
+ * @n: number
+ * Return: prints and void
+ */
+void print_recursion(unsigned long n)
+{
+	print_recursion(n >> 1);
+	_putchar((n & 1) + '0');
+}
+
+/**
+ * print_binary - print binaries
+ * @n: number
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	print_recursion(n);
+}
