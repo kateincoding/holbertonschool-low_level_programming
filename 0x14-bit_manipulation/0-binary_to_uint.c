db3f63345f6c0ff300619d991d61124d9208444c
kateincoding <kateincoding@gmail.com>
2021-03-26 04:35:14 +0000

    changed

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -17,28 +17,25 @@ int _strlen(const char *s)
 /**
  * binary_to_uint - function that converts a char binary to int
  * @b: binary in char
- * @i: marker
  * Return: number int; 0 if b is not binary so b is NULL
- **/
+ */
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int dec = 0, i = 0, res = 0, j = 0, len = _strlen(b);
 
-	if (b == 0)
+	if (!b)
 		return (0);
 	/*condition to check if it is binary or not*/
 	for (j = 0; b[j] != '\0'; j++)
 	{
 		if (b[j] != '0' && b[j] != '1')
-		{
 			return (0);
-		}
 	}
 	/*condition to stop recursion*/
 	while (i <= len - 1)
 	{
 		dec = (b[i] - '0') << (len - i - 1);
- 		res = dec + res;
+		res = dec | res;
 		i++;
 	}
 	return (res);
