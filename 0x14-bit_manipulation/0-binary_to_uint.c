a471040e45bffed16e402cc506e924d0a32fb431
kateincoding <kateincoding@gmail.com>
2021-03-26 04:46:49 +0000

    0-binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,40 +1,28 @@
 #include "holberton.h"
 
 /**
-*_strlen - returns the length of a string
-*@s: array
-*Return: length
-*/
-int _strlen(const char *s)
-{
-	int i = 0;
-
-	while (s[i])
-		i++;
-	return (i);
-}
-
-/**
  * binary_to_uint - function that converts a char binary to int
  * @b: binary in char
  * Return: number int; 0 if b is not binary so b is NULL
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int dec = 0, i = 0, res = 0, j = 0, len = _strlen(b);
+	unsigned int dec = 0, i = 0, res = 0, len;
 
 	if (!b)
 		return (0);
 	/*condition to check if it is binary or not*/
-	for (j = 0; b[j] != '\0'; j++)
+	for (len = 0; b[len] != '\0'; len++)
 	{
-		if (b[j] != '0' && b[j] != '1')
+		if (b[len] != '0' && b[len] != '1')
 			return (0);
 	}
+	*
+	len = len - 1;
 	/*condition to stop recursion*/
-	while (i <= len - 1)
+	while (i <= len)
 	{
-		dec = (b[i] - '0') << (len - i - 1);
+		dec = (b[i] - '0') << (len - i);
 		res = dec | res;
 		i++;
 	}
