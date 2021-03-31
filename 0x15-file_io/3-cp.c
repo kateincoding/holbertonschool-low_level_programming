a90ded79ed49db9b5bd83a8709653f0a352e0ac2
kateincoding <kateincoding@gmail.com>
2021-03-31 05:59:37 +0000

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -27,7 +27,7 @@
  */
 int main(int argc, char **argv)
 {
-	int fd_from, fd_to, from_copy, to_copy;
+	int fd_from, fd_to, from_copy, to_copy, c1, c2;
 	char buffer[1024];
 
 	if (argc != 3)
@@ -45,16 +45,19 @@ int main(int argc, char **argv)
 
 	from_copy = read(fd_from, buffer, 1024);
 	if (from_copy == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 
 	to_copy = write(fd_to, buffer, from_copy);
 	if (from_copy != to_copy)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-	c1 = close(fd_from)
+	c1 = close(fd_from);
 	if (c1 == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
 	c2 = close(fd_to);
 	if (c2 == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdto), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
 	return (0);
 }
