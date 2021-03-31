38a0fbdd437868d816236ab3b19c27316caba06a
kateincoding <kateincoding@gmail.com>
2021-03-31 04:55:55 +0000

    update-

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -31,13 +31,14 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (!text_content)
-		return (-1);
-	while (text_content)
-		len++;
-	num_written = write(fd, text_content, len);
-	if (num_written == -1)
-		return (-1);
+	if (text_content)
+	{
+		while (text_content)
+			len++;
+		num_written = write(fd, text_content, len);
+		if (num_written == -1)
+			return (-1);
+	}
 	close(fd);
 	return (1);
 }
