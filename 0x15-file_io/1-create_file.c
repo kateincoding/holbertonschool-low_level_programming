b119529d92895f883a64a5202f31e5a5bab8caa2
kateincoding <kateincoding@gmail.com>
2021-03-31 04:58:21 +0000

    solve infinite loop

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,14 +28,16 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+
 	if (fd == -1)
 		return (-1);
 
 	if (text_content)
 	{
-		while (text_content)
+		while (text_content[len])
 			len++;
 		num_written = write(fd, text_content, len);
+
 		if (num_written == -1)
 			return (-1);
 	}
