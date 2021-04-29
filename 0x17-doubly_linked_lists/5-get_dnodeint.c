815d29444009dba23bb4b157d023e80eb68bae71
kateincoding <kateincoding@gmail.com>
2021-04-29 16:16:08 +0000

    betty style

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,20 +1,27 @@
 #include "lists.h"
 
+/**
+ * get_dnodeint_at_index - get node of index of the double linked list
+ *
+ * @head: pointer to first node of DLL
+ * @index: index that you want to retire the node
+ * Return: head or null
+ */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-    int count;
+	int count;
 
-    if (!head)
-        return (NULL);
+	if (!head)
+		return (NULL);
 
-    while (head)
-    {
-        head = head->next;
-        count++;
-        if (count == index)
-            return (head); 
-    }
+	while (head)
+	{
+		head = head->next;
+		count++;
+		if (count == index)
+			return (head);
+	}
 
-    if (count <= index)
-        return (NULL);
+	if (count <= index)
+		return (NULL);
 }
