b4a424bd457918abcc626fd7b48ee8904d9745e8
kateincoding <kateincoding@gmail.com>
2021-04-29 16:18:48 +0000

    betty style

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,7 +12,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new_node;
 
 	if (!head)
-        return (NULL);
+		return (NULL);
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
