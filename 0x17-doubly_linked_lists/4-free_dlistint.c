f36d74fef246ac37137d5b62d8ba8c60a9ffb27d
kateincoding <kateincoding@gmail.com>
2021-04-29 15:43:37 +0000

    free a double linked list with not strings

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *head_next;
+
+	while (head)
+	{
+		head_next = head->next;
+		free(head);
+		head = head_next;
+	}
+	free(head);
+}
