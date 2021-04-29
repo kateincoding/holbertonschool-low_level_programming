cb37d3bd74dfd45f76cb6bb192a556575c5af6f8
kateincoding <kateincoding@gmail.com>
2021-04-29 15:09:05 +0000

    adding the case if the DLL is NULL

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * main - check the code for Holberton School students.
+ * add_dnodeint - add a node at the initial of the linked list
  *
  * @head: pointer to pointer to the first node of the double linked list
  * @n: value of the new_node
@@ -9,16 +9,22 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node, *head_tmp;
+	dlistint_t *new_node;
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
 		return (NULL);
 
+	/* initial value of new_node */
 	new_node->n = n;
-	new_node->next = *head;
 	new_node->prev = NULL;
-	(*head)->prev = new_node;
+	if (*head)
+	{
+		new_node->next = *head;
+		/* set the prev of last first node to the new_node */
+		(*head)->prev = new_node;
+	}
+	/* change the head to the address of new_node */
 	*head = new_node;
 	return (*head);
 }
