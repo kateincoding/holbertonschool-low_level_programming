e3c9414955ae08c4dc1465abd1185c46e1de7646
kateincoding <kateincoding@gmail.com>
2021-04-29 16:14:18 +0000

    betty style

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -3,16 +3,17 @@
 /**
  * sum_dlistint - sum all the nodes of double linked list
  *
+ * @head: pointer to first node of DLL
  * Return: Always EXIT_SUCCESS.
  */
 int sum_dlistint(dlistint_t *head)
 {
-    int sum = 0;
+	int sum = 0;
 
-    while (head)
-    {
-        sum = sum + head->n;
-        head = head->next;
-    }
-    return (sum);
+	while (head)
+	{
+		sum = sum + head->n;
+		head = head->next;
+	}
+	return (sum);
 }
