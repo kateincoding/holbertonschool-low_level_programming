69b07fbfa0fca0209142a4085e65f2ea3d1f055d
kateincoding <kateincoding@gmail.com>
2021-05-01 17:30:58 +0000

    adding description of the code in comments

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * free_dlistint - function that frees a dlistint_t list
+ * @head: head of DLL
+ *
+ * Return: void
+ */
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *head_next;
