5c1e5e7f9c1920235125f37f7646c94d0ef3f4d2
kateincoding <kateincoding@gmail.com>
2021-05-04 19:02:38 +0000

    cracking_output

--- 0x18-dynamic_libraries/cracking_output.c
+++ 0x18-dynamic_libraries/cracking_output.c
@@ -1,7 +1,11 @@
 #include <unistd.h>
 
+int printf(const char *format, ...)
+{
+    return (0);
+}
+
 int puts (const char *s)
 {
-    write(1, "Congratulations, you win the Jackpot!\n", 38);
-    return (0);
+    return (write(1, "9 8 10 24 75 - 9\nCongratulations, you win the Jackpot!\n", 57));
 }
