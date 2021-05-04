45950ba090ee39591fde7120126142f1ac2c9a53
kateincoding <kateincoding@gmail.com>
2021-05-04 06:04:49 +0000

    raw link

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -qO /tmp/cracking_output.so https://raw.githubusercontent.com/kateincoding/holbertonschool-low_level_programming/main/0x18-dynamic_libraries/cracking_output.so
+wget -qO /tmp/cracking_output.so https://raw.github.com/kateincoding/holbertonschool-low_level_programming/main/0x18-dynamic_libraries/cracking_output.so
 export LD_PRELOAD=/tmp/cracking_output.so
