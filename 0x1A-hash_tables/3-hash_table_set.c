81a051e3b343a36f53eb28e0f302a75f1d1c4a9f
kateincoding <kateincoding@gmail.com>
2021-05-28 19:31:52 +0000

    rest

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *hash_node = NULL, *new_node = NULL;
 	char *new_value = NULL, *new_key = NULL;
 
-	if (!ht || !(ht->size) || !key || !key[0])
+	if (!ht || !(ht->array) || !key || !key[0])
 		return (0);
 
 	idx = key_index((const unsigned char *)key, ht->size);
