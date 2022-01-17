cab35de49c4728da07b398a04f63e81375470d6c
kateincoding <kateincoding@gmail.com>
2022-01-17 17:51:34 -0500

    [enter printfs in the function]

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -8,9 +8,10 @@ int linear_search(int *array, size_t size, int value)
 	size_t i;
 
 	if (!array)
-		return (-1)
+		return (-1);
 	for(i = 0; i < size; i++)
 	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return(i);
 	}
