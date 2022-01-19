04d7e0300b502d7fa56c9da5c5644af25c32690a
kateincoding <kateincoding@gmail.com>
2022-01-19 05:48:58 -0500

    [change order of validation restriction]

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -30,7 +30,7 @@ int jump_search(int *array, size_t size, int value)
 	if ((int)i > (size - 1))
 		i = size - 1;
 	/* Linear search */
-	while (array[prev] <= value && prev <= i)
+	while (prev <= i && array[prev] <= value)
 	{
 		printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
 		if (array[prev] == value)
