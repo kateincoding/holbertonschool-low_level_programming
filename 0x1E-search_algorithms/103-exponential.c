0c4d72d5561f23708fa28ae4e37f9cd1fb66171a
kateincoding <kateincoding@gmail.com>
2022-01-19 12:40:49 -0500

    [betty]

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,6 +1,5 @@
 #include "search_algos.h"
 
-
 /**
  * print_scope_x - Print the scope of the array to search
  * @array: pointer to array
@@ -56,23 +55,23 @@ int binary_search_range(int *array, size_t left, size_t right, int value)
  */
 int exponential_search(int *array, size_t size, int value)
 {
-    int i;
-    int high;
+	int i;
+	int high;
 
-    if (!array || size == 0)
-        return -1;
-    if (array[0] == value)
-        return 0;
-    i = 1;
-    while (i < value && array[i] <= value)
-    {
-        printf("Value checked array[%d] = [%d]\n", i, array[i]);
-        i = i * 2;
-    }
-    if (i < ((int)size - 1))
-        high = i;
-    else
-        high = size - 1;
-    printf("Value found between indexes [%d] and [%d]\n", i / 2, high);
-    return (binary_search_range(array, i/2, high, value));
+	if (!array || size == 0)
+		return (-1);
+	if (array[0] == value)
+		return (0);
+	i = 1;
+	while (i < value && array[i] <= value)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		i = i * 2;
+	}
+	if (i < ((int)size - 1))
+		high = i;
+	else
+		high = size - 1;
+	printf("Value found between indexes [%d] and [%d]\n", i / 2, high);
+	return (binary_search_range(array, i / 2, high, value));
 }
